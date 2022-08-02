36b30fe893293d546ece4cd0096c3c175b2bfa44
Devin Garcia <devingarcia5423@gmail.com>
2022-08-02 12:15:57 -0400

    added links to each game preview

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -1,24 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import GameCard from './GameCard';
+import GamePreview from './GamePreview';
 
 function GameList() {
   const [games, setGames] = useState([]);
+  const [isClicked, setIsClicked] = useState(false);
 
   useEffect(() => {
     fetch('/games')
       .then((res) => res.json())
       .then((data) => setGames(data));
   }, []);
+
   return (
     <div className='game-container'>
       {games.map((game) => (
-        <GameCard
+        <GamePreview
           key={game.id}
           id={game.id}
           title={game.title}
           genre={game.genre}
           platform={game.platform}
           image={game.image}
+          isClicked={isClicked}
+          setIsClicked={setIsClicked}
         />
       ))}
     </div>
