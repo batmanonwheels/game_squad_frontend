1c09d5a0f3171ed93e32d3833d5aa891ba99bff7
James Macapagal <j.macapagal@gmail.com>
2022-08-03 17:39:03 -0500

    added Review Post form

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -2,19 +2,42 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import '../assets/css/GameInfo.css';
 
-function GameInfo() {
+function GameInfo({user}) {
   const { id } = useParams();
   const [gameData, setGameData] = useState({});
+  const [title,setTitle] = useState("");
+  const [description, setDescription] = useState("")
+  const [rating, setRating] = useState(0)
+
 
   useEffect(() => {
     fetch(`/games/${id}`)
       .then((res) => res.json())
       .then((data) => setGameData(data));
   }, []);
-
-  console.log(gameData);
+    function handleSubmitReview(e){
+        e.preventDefault()
+        const reviewObj = {
+            title: title,
+            description: description,
+            upvotes:0,
+            downvotes:0,
+            rating: rating,
+            game_id: id,
+            user_id: user.id
+        }
+        fetch('/reviews', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(reviewObj)
+        }).then((r) => r.json())
+        .then((data)=> console.log(data))
+    }
 
   return (
+    <>
     <div className='game-info'>
       <img
         className='game-info-image'
@@ -30,6 +53,32 @@ function GameInfo() {
         }};
       </ul> */}
     </div>
+    <form onSubmit={(e)=>{handleSubmitReview(e)}}>
+        <label>title</label>
+        <input
+            placeholder="Give Your Review A Title"
+            type="text"
+            onChange={(e)=>setTitle(e.target.value)}
+            value={title}
+        ></input>
+        <label>review text</label>
+        <textarea 
+            placeholder="Write Your Review"
+            type="text"
+            onChange={(e)=>setDescription(e.target.value)}
+            value={description}
+        ></textarea>
+        <label>rate</label>
+        <select id="rating" name="rating" onChange={(e)=>setRating(e.target.value)} value={rating}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
+        <button>Submit Review</button>
+    </form>
+    </>
   );
 }
 
